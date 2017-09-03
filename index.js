a6b5214d7b0442cc326a8ee1d070ca18e9157f47
hhao99 <hhao99@163.com>
2017-09-04 07:44:38 +0800

    modify the index.js

--- index.js
+++ index.js
@@ -1,42 +1,9 @@
-'use strict'
+const Koa = require('koa')
 
-//load .env in the local development
-if (process.env.NODE_ENV === 'development') {
-    require('dotnet').config({ silent: true})
-}
+const app = new Koa();
 
-const logger = require('winston')
-const semver = require('semver')
-const pkg = require('./package.json')
+app.use(async ctx=> {
+    ctx.body = 'Hello World';
+});
 
-// validate the node version requirement
-
-const runtime = {
-    expected: semver.validRange(pkg.engines.node),
-    actual: semver.valid(process.version)
-}
-
-const valid = semver.satisfies(runtime.actual, runtime.expected)
-if (!valid) {
-    throw new Error(`Expected Node.js version ${runtime.expected}, but fond v${runtime.actual}. Please update or change your runtime`)
-}
-
-// configure the logger
-logger.default.transports.console.colorize = true
-logger.default.transports.console.timestamp = true
-logger.default.transports.console.prettyPrint = config.env === 'development'
-logger.level = config.logger.level
-
-//start process
-logger.info(`Starting $(config.process.type} process`, { pid: process.pid})
-
-if(config.process.tyoe === 'web') {
-    require('./web')
-}
-else if (config.process.type === 'worker') {
-    require('./worker')
-}
-else {
-    throw new Error(`${config.process.type} is an unsupported process type.`)
-
-}
+app.listen(3000)
